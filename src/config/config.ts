7d10b66953ac66ecdeebeacdc04fec3ccc864525
Jairos Nyirenda <jairos.pemphonyirenda@gmail.com>
2022-09-18 00:59:49 +0200

    fixing configurations

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,16 +1,16 @@
-require('dotenv').config()
+//require('dotenv').config()
 
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'dq1x3akhq',
+  'password': 'dq1x3akhq',
+  'database': 'udagram_microservice',
+  'host': 'udagram-jairos-microservice-rds.cpyintdrpy6j.us-east-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'udagram-s3-jairos-nyirenda',
+  'url': 'http://localhost:8000',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret':'CIOPHASFHPOIQHEWPUIDHFQP',
   },
 };
